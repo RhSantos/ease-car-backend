9e620a5ec5a0850d8ce71b820f0aa40132436a9d
RhSantos <rhuan.santos.araujo@gmail.com>
2024-04-04 11:45:39 -0300

    chore: Address Model to Authentication App

--- api/models.py
+++ api/models.py
@@ -7,19 +7,6 @@ from django.utils import timezone
 from django.utils.safestring import mark_safe
 
 
-class Address(models.Model):
-    street = models.CharField(max_length=100)
-    number = models.IntegerField()
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    postal_code = models.CharField(max_length=20)
-    country = models.CharField(max_length=50, default="Brasil")
-    complement = models.CharField(max_length=100, blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.street}, {self.city}, {self.state} {self.postal_code}, {self.country}"
-
-
 class ProfileUser(AbstractUser):
     profile_pic = models.ImageField(upload_to="user/")
     address = models.ForeignKey(
--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import BaseUserManager
+from django.db import models
+
 
 class ProfileUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -18,3 +19,16 @@ class ProfileUserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
 
         return self.create_user(email, password, **extra_fields)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    number = models.IntegerField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=50, default="Brasil")
+    complement = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.state} {self.postal_code}, {self.country}"
