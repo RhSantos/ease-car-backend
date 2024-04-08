13336f04a105b4c196f05251e95ead4452ec544c
RhSantos <rhuan.santos.araujo@gmail.com>
2024-04-08 20:12:34 -0300

    chore: Brand Responses Fix

--- api/routes/brand/views.py
+++ api/routes/brand/views.py
@@ -1,9 +1,8 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
 
 from api.models import Brand
 from api.serializers import BrandSerializer
-from api.utils.jsend_responses import success_response
+from utils.jsend_responses import success_response
 
 
 class BrandViewSet(viewsets.ModelViewSet):
@@ -14,4 +13,4 @@ class BrandViewSet(viewsets.ModelViewSet):
     def list(self, request):
         brands = Brand.objects.all()
         serializer = BrandSerializer(brands, many=True)
-        return success_response({"brands": serializer.data})
+        return success_response(key="brands", data=serializer.data)
