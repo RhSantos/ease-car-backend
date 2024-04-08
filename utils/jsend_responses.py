87933ff3cdd2116a33de613c26621a232016a086
RhSantos <rhuan.santos.araujo@gmail.com>
2024-04-08 17:46:32 -0300

    chore: Jsend Responses - Success Resp Obj Key

--- utils/jsend_responses.py
+++ utils/jsend_responses.py
@@ -1,13 +1,16 @@
-from rest_framework.response import Response
 from rest_framework import status
-
-def success_response(data=None, status=status.HTTP_200_OK):
-    return Response({"status": "success", "data": data}, status = status)
+from rest_framework.response import Response
 
 
-def fail_response(errors, status = status.HTTP_400_BAD_REQUEST):
-    return Response({"status": "fail", "data": errors}, status = status)
+def success_response(key="object", data=None, status=status.HTTP_200_OK):
+    return Response(
+        {"status": "success", "data": ({key: data} if data else None)}, status=status
+    )
 
 
-def error_response(message, status = status.HTTP_404_NOT_FOUND):
-    return Response({"status": "error", "message": message}, status = status)
+def fail_response(errors, status=status.HTTP_400_BAD_REQUEST):
+    return Response({"status": "fail", "data": errors}, status=status)
+
+
+def error_response(message, status=status.HTTP_404_NOT_FOUND):
+    return Response({"status": "error", "message": message}, status=status)
