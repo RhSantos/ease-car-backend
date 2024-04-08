8bc11ca182cc1a75e933cb04974d95bee8e4a665
RhSantos <rhuan.santos.araujo@gmail.com>
2024-04-08 20:13:00 -0300

    chore: Rental Responses Fix

--- api/routes/rental/views.py
+++ api/routes/rental/views.py
@@ -1,10 +1,9 @@
 from django.http.response import Http404
 from rest_framework import status, viewsets
-from rest_framework.response import Response
 
 from api.models import Rental
 from api.serializers import RentalSerializer
-from api.utils.jsend_responses import error_response, fail_response, success_response
+from utils.jsend_responses import *
 
 
 class RentalViewSet(viewsets.ModelViewSet):
@@ -14,15 +13,14 @@ class RentalViewSet(viewsets.ModelViewSet):
     def list(self, request):
         rentals = Rental.objects.all()
         serializer = RentalSerializer(rentals, many=True)
-        return success_response({"rentals": serializer.data})
+        return success_response(key="rentals", data=serializer.data)
 
     def retrieve(self, request, pk=None):
         try:
             rental = self.get_object()
             serializer = RentalSerializer(rental)
-            return Response(serializer.data)
-        except Exception as e:
-            print(e.__class__)
+            return success_response(key="rental", data=serializer.data)
+        except Http404:
             return error_response("Rental not found")
 
     def create(self, request):
@@ -30,7 +28,7 @@ class RentalViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             serializer.save()
             return success_response(
-                {"rental": serializer.data}, status=status.HTTP_201_CREATED
+                key="rental", data=serializer.data, status=status.HTTP_201_CREATED
             )
         return fail_response(serializer.errors)
 
@@ -43,7 +41,7 @@ class RentalViewSet(viewsets.ModelViewSet):
         serializer = RentalSerializer(rental, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return success_response(key="rental", data=serializer.data)
         return fail_response(serializer.errors)
 
     def destroy(self, request, pk=None):
