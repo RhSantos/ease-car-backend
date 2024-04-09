3fa9b0eff8c4596af9103412ad8627916bf63208
RhSantos <rhuan.santos.araujo@gmail.com>
2024-04-09 16:32:33 -0300

    chore: Brand Permissions

--- api/routes/brand/views.py
+++ api/routes/brand/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 from api.models import Brand
 from api.serializers import BrandSerializer
@@ -9,6 +10,16 @@ class BrandViewSet(viewsets.ModelViewSet):
 
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
+
+    def get_permissions(self):
+
+        admin_only = ["create", "update", "destroy"]
+
+        if self.action in admin_only:
+            return [
+                IsAdminUser(),
+            ]
+        return []
 
     def list(self, request):
         brands = Brand.objects.all()
