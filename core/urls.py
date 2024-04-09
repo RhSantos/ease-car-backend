4667906620e526f3be4c25227a438304b8da9b7f
RhSantos <rhuan.santos.araujo@gmail.com>
2024-04-09 14:08:41 -0300

    feat: URL Route - [Login]

--- core/urls.py
+++ core/urls.py
@@ -2,15 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from authentication.views import RegisterAPIView
+from authentication.views import LoginAPIView, RegisterAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/login/", TokenObtainPairView.as_view()),
     path("auth/refresh-token/", TokenRefreshView.as_view()),
     path("auth/register/", RegisterAPIView.as_view()),
+    path("auth/login/", LoginAPIView.as_view()),
     path("api/", include("api.urls")),
 ]
 
