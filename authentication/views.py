bb921b5555d48b02d17902b5ddfb2fb721682469
RhSantos <rhuan.santos.araujo@gmail.com>
2024-04-08 23:43:56 -0300

    feat: Registration Api View

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from utils.jsend_responses import *
+
+from .models import Address
+from .serializers import ProfileUserSerializer
+
+
+class RegisterAPIView(CreateAPIView):
+    serializer_class = ProfileUserSerializer
+
+    def post(self, request):
+        serializer = ProfileUserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return success_response(
+                key="user", data=serializer.data, status=status.HTTP_201_CREATED
+            )
+        return fail_response(serializer.errors)
