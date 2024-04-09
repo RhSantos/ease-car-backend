8ee38c485ae6dffdbe78abfc15f4ef05e8e22f65
RhSantos <rhuan.santos.araujo@gmail.com>
2024-04-09 14:08:20 -0300

    feat: Login Api View

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,11 @@
 from rest_framework import status
 from rest_framework.generics import CreateAPIView
-
+from rest_framework_simplejwt.tokens import RefreshToken
+from authentication.models import ProfileUser
 from utils.jsend_responses import *
 
 from .models import Address
-from .serializers import RegisterSerializer
+from .serializers import LoginSerializer, RegisterSerializer
 
 
 class RegisterAPIView(CreateAPIView):
@@ -18,4 +19,35 @@ class RegisterAPIView(CreateAPIView):
             return success_response(
                 key="user", data=serializer.data, status=status.HTTP_201_CREATED
             )
+        return fail_response(serializer.errors)
+
+
+class LoginAPIView(CreateAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+            email = request.data.get("email")
+            password = request.data.get("password")
+            user = ProfileUser.objects.get(email=email)
+
+            if not user:
+                return error_response("User not found")
+
+            if user.check_password(password):
+
+                refresh_token = RefreshToken.for_user(user)
+                user_serializer = RegisterSerializer(user)
+
+                data = {
+                    "user": user_serializer.data,
+                    "token": str(refresh_token),
+                    "refresh-token": str(refresh_token.access_token),
+                }
+
+                return success_response(
+                    key="auth", data=data, status=status.HTTP_201_CREATED
+                )
         return fail_response(serializer.errors)
