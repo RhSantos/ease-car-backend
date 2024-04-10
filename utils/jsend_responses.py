2ee3e6c4cb960f3fc59214108d2d23a4208f0f5c
RhSantos <rhuan.santos.araujo@gmail.com>
2024-04-10 16:45:07 -0300

    chore: Jsend Fail Response Change

--- utils/jsend_responses.py
+++ utils/jsend_responses.py
@@ -9,7 +9,12 @@ def success_response(key="object", data=None, status=status.HTTP_200_OK):
 
 
 def fail_response(errors, status=status.HTTP_400_BAD_REQUEST):
-    return Response({"status": "fail", "data": errors}, status=status)
+    valid_errors = {
+        key: value if type(value) != list and len(value) != 1 else value[0]
+        for key,value in errors.items()
+    }
+
+    return Response({"status": "fail", "data": valid_errors}, status=status)
 
 
 def error_response(message, status=status.HTTP_404_NOT_FOUND):
