1ee599785c279803346ed8cd28e282520779c2a5
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 17:31:59 -0300

    chore: Rental Admin - [Rental App]

--- core/rental/admin.py
+++ core/rental/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Rental
+
+
+@admin.register(Rental)
+class RentalAdmin(admin.ModelAdmin):
+    all_fields = [f.name for f in Rental._meta.fields]
+    parent_fields = Rental.get_deferred_fields(Rental)
+
+    list_display = all_fields
+    read_only = parent_fields
