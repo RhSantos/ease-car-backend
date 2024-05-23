05059cd9ad9bb60c6cb27e089676e54187d98873
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 18:36:29 -0300

    chore: Payment Views (ViewSet) - [Payment App]

--- core/payment/views.py
+++ core/payment/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
-# Create your views here.
+from core.general.utils.responses import *
+
+from .models import Payment
+from .serializers import PaymentResponseSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+
+    queryset = Payment.objects.all()
+    serializer_class = PaymentResponseSerializer
+
+    def get_permissions(self):
+
+        admin_only = ["create", "update", "destroy"]
+
+        if self.action in admin_only:
+            return [
+                IsAdminUser(),
+            ]
+        return [
+            IsAuthenticated(),
+        ]
+
+    def list(self, request):
+        payments = Payment.objects.filter(owner=request.user)
+        serializer = PaymentResponseSerializer(payments, many=True)
+        return success_response(key="payments", data=serializer.data)
+
+    def retrieve(self, request, pk=None):
+        try:
+            payment = self.get_object()
+
+            if payment.owner == request.user:
+                serializer = PaymentResponseSerializer(payment)
+                return success_response(key="payment", data=serializer.data)
+
+            return fail_response(
+                errors={"user": "You are not Payment Owner"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+        except Http404:
+            return error_response("Payment not found")
