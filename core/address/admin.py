8e7711e3832a019b5709ded514d95b6448ebc4c9
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 15:23:01 -0300

    chore: Address Admin - [Address App]

--- core/address/admin.py
+++ core/address/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Address
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    all_fields = [f.name for f in Address._meta.fields]
+    parent_fields = Address.get_deferred_fields(Address)
+
+    list_display = all_fields
+    read_only = parent_fields
