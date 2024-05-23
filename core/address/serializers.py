6159f2de6d666a295c7f02c1a488c9e9b8e87f3e
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 15:24:42 -0300

    chore: Address Serializer - [Address App]

---
+++ core/address/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from .models import Address
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = [
+            "id",
+            "street",
+            "number",
+            "city",
+            "state",
+            "postal_code",
+            "country",
+            "complement",
+        ]
