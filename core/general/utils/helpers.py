42c963e4786e59b65326f8afd4b8a802e1c11c33
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 19:17:24 -0300

    feat: Formatting Multipart Form Data Function - helpers.py

---
+++ core/general/utils/helpers.py
@@ -0,0 +1,17 @@
+import json
+
+
+def format_multipart_form_data_field(data, field: str):
+    temp_data = data.copy()
+    if temp_data.get(field):
+        try:
+            data_field = json.loads(temp_data.get(field))
+            if data_field:
+                for key, value in data_field.items():
+                    temp_data[f"{field}.{key}"] = value
+
+            temp_data[field] = None
+        except:
+            return None
+
+    return temp_data
