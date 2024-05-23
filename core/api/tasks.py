5840ed1bae97152f16acc18aec7ffa0d98e9e5ab
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 18:55:20 -0300

    chore: Api App Deleted

--- core/api/tasks.py
+++
@@ -1,9 +0,0 @@
-from background_task import background
-from .models import Payment
-
-@background(schedule=5)
-def confirm_booking_payment(payment_id:str):
-    payment = Payment.objects.get(id=payment_id)
-    payment.payment_status = Payment.PaymentStatus.COMPLETED
-
-    payment.save()
