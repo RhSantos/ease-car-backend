df4170ad0995ddb5630769df8b8793e01d6fe618
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 17:34:47 -0300

    chore: Rental Views (ViewSet) - [Rental App]

--- core/rental/views.py
+++ core/rental/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from rest_framework import status, viewsets
 
-# Create your views here.
+from core.general.utils.responses import *
+
+from .models import Rental
+from .serializers import RentalSerializer
+
+
+class RentalViewSet(viewsets.ModelViewSet):
+    queryset = Rental.objects.all()
+    serializer_class = RentalSerializer
+
+    def list(self, request):
+        rentals = Rental.objects.all()
+        serializer = RentalSerializer(rentals, many=True)
+        return success_response(key="rentals", data=serializer.data)
+
+    def retrieve(self, request, pk=None):
+        try:
+            rental = self.get_object()
+            serializer = RentalSerializer(rental)
+            return success_response(key="rental", data=serializer.data)
+        except Http404:
+            return error_response("Rental not found")
+
+    def create(self, request):
+        serializer = RentalSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return success_response(
+                key="rental", data=serializer.data, status=status.HTTP_201_CREATED
+            )
+        return fail_response(serializer.errors)
+
+    def update(self, request, pk=None):
+        try:
+            rental = self.get_object()
+        except Http404:
+            return error_response("Rental not found")
+
+        serializer = RentalSerializer(rental, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return success_response(key="rental", data=serializer.data)
+        return fail_response(serializer.errors)
+
+    def destroy(self, request, pk=None):
+        try:
+            rental = self.get_object()
+            rental.delete()
+        except Http404:
+            return error_response("Rental not found")
+        return success_response()
