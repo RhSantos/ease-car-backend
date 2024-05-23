db0ee0cde6ca8e79937a4c18e9ef3a395aa4e3c0
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 18:47:07 -0300

    chore: Booking Admin - [Booking App]

--- core/booking/admin.py
+++ core/booking/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Booking
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    all_fields = [f.name for f in Booking._meta.fields]
+    parent_fields = Booking.get_deferred_fields(Booking)
+
+    list_display = all_fields
+    read_only = parent_fields
