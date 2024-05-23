c523fe55377a822ffed6f499bc5386c14c5e97a3
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 18:13:41 -0300

    chore: Review Views (ViewSet) - [Review App]

--- core/review/views.py
+++ core/review/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from rest_framework import status, viewsets
 
-# Create your views here.
+from core.general.utils.responses import *
+
+from .models import Review
+from .serializers import ReviewSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def list(self, request):
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return success_response(key="reviews", data=serializer.data)
+
+    def retrieve(self, request, pk=None):
+        try:
+            review = self.get_object()
+            serializer = ReviewSerializer(review)
+            return success_response(key="review", data=serializer.data)
+        except Exception:
+            return error_response("Review not found")
+
+    def create(self, request):
+        serializer = ReviewSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return success_response(
+                key="review", data=serializer.data, status=status.HTTP_201_CREATED
+            )
+        return fail_response(serializer.errors)
+
+    def update(self, request, pk=None):
+        try:
+            review = self.get_object()
+        except Http404:
+            return error_response("Review not found")
+
+        serializer = ReviewSerializer(review, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return success_response(key="review", data=serializer.data)
+        return fail_response(serializer.errors)
+
+    def destroy(self, request, pk=None):
+        try:
+            review = self.get_object()
+            review.delete()
+        except Http404:
+            return error_response("Review not found")
+        return success_response()
