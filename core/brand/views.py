2c84b11478761078d78b3743eea9c3e5fc1bf048
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 16:42:25 -0300

    chore: Brand Views (ViewSet) - [Brand App]

--- core/brand/views.py
+++ core/brand/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from core.general.utils.responses import success_response
+
+from .models import Brand
+from .serializers import BrandSerializer
+
+
+class BrandViewSet(viewsets.ModelViewSet):
+
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+    def get_permissions(self):
+
+        admin_only = ["create", "update", "destroy"]
+
+        if self.action in admin_only:
+            return [
+                IsAdminUser(),
+            ]
+        return []
+
+    def list(self, request):
+        brands = Brand.objects.all()
+        serializer = BrandSerializer(brands, many=True)
+        return success_response(key="brands", data=serializer.data)
