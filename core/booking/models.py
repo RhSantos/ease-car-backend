17c2356937a7b9811399da2a7a3a705431c206bc
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 18:45:28 -0300

    chore: Booking Model - [Booking App]

--- core/booking/models.py
+++ core/booking/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from core.address.models import Address
+from core.authentication.models import ProfileUser
+from core.payment.models import Payment
+from core.rental.models import Rental
+
+
+class Booking(models.Model):
+
+    renter = models.ForeignKey(ProfileUser, on_delete=models.CASCADE)
+    rental = models.ForeignKey(Rental, on_delete=models.CASCADE)
+    location = models.ForeignKey(Address, on_delete=models.CASCADE)
+    rent_date = models.DateTimeField()
+    return_date = models.DateTimeField()
+    payments = models.ManyToManyField(Payment)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.updated_at = timezone.now()
+        super(Booking, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.id} [{self.renter} - {self.rental}]"
