e2524bc1fb7d20d59eab3e5ecf25a9bc9670c579
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 15:17:49 -0300

    chore: Address Model - [Address App]

--- core/address/models.py
+++ core/address/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    number = models.IntegerField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=50, default="Brasil")
+    complement = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.state} {self.postal_code}, {self.country}"
