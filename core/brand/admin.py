1b28d775c2bc71b39ed95329229ee79176d48dbf
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 16:40:20 -0300

    chore: Brand Admin - [Brand App]

--- core/brand/admin.py
+++ core/brand/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Brand
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    fields = ["name", "image_preview", "image"]
+    list_display = ["name", "image_preview"]
+    readonly_fields = ["image_preview"]
