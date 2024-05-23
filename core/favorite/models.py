71f45e8e6cf275a03b0651b8bc707212acfdca02
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 18:21:23 -0300

    chore: Favorite Model - [Favorite App]

--- core/favorite/models.py
+++ core/favorite/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from core.authentication.models import ProfileUser
+from core.rental.models import Rental
+
+
+class Favorite(models.Model):
+
+    owner = models.ForeignKey(ProfileUser, on_delete=models.CASCADE)
+    rental = models.ForeignKey(Rental, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.updated_at = timezone.now()
+        super(Favorite, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.user} [{self.rental}]"
