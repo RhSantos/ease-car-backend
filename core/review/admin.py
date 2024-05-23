66af217fd5b392bded4bf5b49a8c22e48903189a
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 18:10:41 -0300

    chore: Review Admin - [Review App]

--- core/review/admin.py
+++ core/review/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    all_fields = [f.name for f in Review._meta.fields]
+    parent_fields = Review.get_deferred_fields(Review)
+
+    list_display = all_fields
+    read_only = parent_fields
