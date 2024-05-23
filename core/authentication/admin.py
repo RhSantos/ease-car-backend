3f39520b70dbddf68c02b191944b91b1bb54892b
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 20:05:35 -0300

    fix: Removing duplicated Admin Registrations - Old Registers

--- core/authentication/admin.py
+++ core/authentication/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
 
-from .models import Address, ProfileUser
-
-
-@admin.register(Address)
-class AddressAdmin(admin.ModelAdmin):
-    all_fields = [f.name for f in Address._meta.fields]
-    parent_fields = Address.get_deferred_fields(Address)
-
-    list_display = all_fields
-    read_only = parent_fields
+from .models import ProfileUser
 
 
 @admin.register(ProfileUser)
