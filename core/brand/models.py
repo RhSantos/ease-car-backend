c42ab52a5bb3ebe2d5d387b6823d5b7fa78b68a3
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-23 16:38:51 -0300

    chore: Brand Model - [Brand App]

--- core/brand/models.py
+++ core/brand/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="brand/")
+
+    def __str__(self):
+        return self.name
+
+    def image_preview(self):
+        return mark_safe('<img src="%s"/>' % self.image.url)
+
+    image_preview.allow_tags = True
