314461a3ed8c40f67ba63e0b6776f7d546621a9d
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-28 10:42:30 -0300

    chore: Changing Env Vars Settings Prefix

--- core/project/settings/__init__.py
+++ core/project/settings/__init__.py
@@ -6,7 +6,7 @@ from split_settings.tools import include, optional
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 # Namespacing our own environment variables
-ENVVAR_SETTINGS_PREFIX = "CORESETTINGS_"
+ENVVAR_SETTINGS_PREFIX = "EASE_CAR_SETTINGS_"
 
 LOCAL_SETTINGS_PATH = os.getenv(f"{ENVVAR_SETTINGS_PREFIX}LOCAL_SETTINGS_PATH")
 
