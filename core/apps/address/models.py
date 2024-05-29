a0303971383a4a1eda61173510f4dc58f68dcbe9
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-28 23:06:22 -0300

    chore: Province field in Address Model

--- core/apps/address/models.py
+++ core/apps/address/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Address(models.Model):
     street = models.CharField(max_length=100)
     number = models.IntegerField()
+    province = models.CharField(max_length=70)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
     postal_code = models.CharField(max_length=20)
@@ -11,4 +12,4 @@ class Address(models.Model):
     complement = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.street}, {self.city}, {self.state} {self.postal_code}, {self.country}"
+        return f"{self.street}, {self.number} - {self.city}, {self.country}"
