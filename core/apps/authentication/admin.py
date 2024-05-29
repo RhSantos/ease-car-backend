b0bda65d6f9d8c3ff4134bc75614031c6891bb74
RhSantos <rhuan.santos.araujo@gmail.com>
2024-05-28 23:07:43 -0300

    chore: Profile User Admin new fields added

--- core/apps/authentication/admin.py
+++ core/apps/authentication/admin.py
@@ -5,5 +5,16 @@ from .models import ProfileUser
 
 @admin.register(ProfileUser)
 class ProfileUserAdmin(admin.ModelAdmin):
-    fields = ["username", "email", "first_name", "last_name", "profile_pic", "address"]
+    fields = [
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "cpf",
+        "profile_pic",
+        "birth_date",
+        "mobile_phone",
+        "is_premium",
+        "address",
+    ]
     list_display = ["id", "thumbnail", "username", "email"]
