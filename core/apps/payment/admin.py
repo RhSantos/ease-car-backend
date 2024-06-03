90134a91cd2b55ad2fa32dd9302b41cce4b21b49
RhSantos <rhuan.santos.araujo@gmail.com>
2024-06-03 13:59:39 -0300

    feat: Payment Customer Admin Register

--- core/apps/payment/admin.py
+++ core/apps/payment/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 
-from .models import SubAccount
+from .models import SubAccount, Customer
 
 
 @admin.register(SubAccount)
 class SubAccountAdmin(admin.ModelAdmin):
     all_fields = [f.name for f in SubAccount._meta.fields]
     parent_fields = SubAccount.get_deferred_fields(SubAccount)
+
+    list_display = all_fields
+    read_only = parent_fields
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    all_fields = [f.name for f in Customer._meta.fields]
+    parent_fields = Customer.get_deferred_fields(Customer)
 
     list_display = all_fields
     read_only = parent_fields
