a258a502ec67660ad59f7a82132cea62dbfb28be
RhSantos <rhuan.santos.araujo@gmail.com>
2024-06-20 10:39:06 -0300

    chore: Asaas Api Call Http Method Argument

--- core/general/utils/network.py
+++ core/general/utils/network.py
@@ -1,11 +1,16 @@
+from http import HTTPMethod
+
 import requests
 from django.conf import settings
+
 from core.general.utils.helpers import AsaasResourceUrl
 
 
-def make_asaas_api_call(request_data: dict, api_resource:AsaasResourceUrl):
-
-    response = requests.post(
+def make_asaas_api_call(
+    method: HTTPMethod, request_data: dict, api_resource: AsaasResourceUrl
+):
+    response = requests.request(
+        method=method.value,
         url=settings.ASAAS_API_URL + str(api_resource),
         json=request_data,
         headers={
